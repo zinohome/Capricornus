5ecc3b0aba5815be4b2ef516f78b5c736addee8e
zhangjun <ibmzhangjun@139.com>
2022-04-21 15:04:14 +0800

    Pre release

---
+++ config/hypercorn.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+bind = "0.0.0.0:8843"
+insecure-bind = "0.0.0.0:8880"
+keyfile = "/Users/zhangjun/PycharmProjects/Capricornus/cert/key.pem"
+certfile = "/Users/zhangjun/PycharmProjects/Capricornus/cert/cert.pem"
