9c81cb78094effcefe468c66917536c7aa424986
zhangjun <ibmzhangjun@139.com>
2022-04-22 16:13:44 +0800

    mysql add primary key

--- config/dbddl.sql
+++ config/dbddl.sql
@@ -1,26 +1,28 @@
 
-CREATE TABLE `ogdb` (
+CREATE TABLE `ogdbconnect` (
 	`ogdb_id` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
 	`ogdb_name` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
 	PRIMARY KEY  `(ogdb_id`) 
-)DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci ENGINE=InnoDB
+)COLLATE utf8mb4_general_ci DEFAULT CHARSET=utf8mb4 ENGINE=InnoDB
 
 
 CREATE TABLE `ogdbuser` (
 	`ogdbuser_id` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
 	`ogdbuser_name` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
 	`ogdbuser_password` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
-	`ogdbuser_active` TINYINT(1) NOT NULL, 
-	`ogdb_id` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL
-)DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci ENGINE=InnoDB
+	`ogdbuser_active` VARCHAR(10) COLLATE utf8mb4_general_ci NOT NULL, 
+	`ogdb_id` VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
+	PRIMARY KEY  `(ogdbuser_id`) 
+)COLLATE utf8mb4_general_ci DEFAULT CHARSET=utf8mb4 ENGINE=InnoDB
 
 
 CREATE TABLE `sysuser` (
-	userid BIGINT NOT NULL AUTO_INCREMENT, 
-	username VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
-	userpass VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
-	nickname VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL, 
-	userrole VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user', 
-	PRIMARY KEY (userid)
-)DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci ENGINE=InnoDB
+	`sysuser_id` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	`sysuser_name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	`sysuser_password` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	`sysuser_role` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	`sysuser_isactive` VARCHAR(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	`sysuser_nickname` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, 
+	PRIMARY KEY  `(sysuser_id`) 
+)COLLATE utf8mb4_general_ci DEFAULT CHARSET=utf8mb4 ENGINE=InnoDB
 
