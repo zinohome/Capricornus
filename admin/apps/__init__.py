0abb1f1eb2da1e92c84d69e7a31770a4387225b3
zhangjun <ibmzhangjun@139.com>
2022-05-27 11:50:33 +0800

    update to v2.1.5

--- admin/apps/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from flask import Flask
-from flask_login import LoginManager
-
-login_manager = LoginManager()
--- docker/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IMGNAME=jointhero/capricornus
-IMGVERSION=v2.1.2
-docker build --no-cache -t $IMGNAME:$IMGVERSION .
