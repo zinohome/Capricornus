5ff9a3bdb98c3bf89a178b99a47e91c2188db3d8
zhangjun <ibmzhangjun@139.com>
2022-05-30 08:33:38 +0800

    update to v2.1.5

--- docker/bd_build/50_start_capricornus.sh
+++ docker/bd_build/50_start_capricornus.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-FIND_FILE="/opt/Capricornus/config/settings.ini"
-FIND_STR="settings"
-if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
-    echo "settings exist"
-else
-    cp /opt/Capricornus/config/settings_default.ini /opt/Capricornus/config/settings.ini
-fi
 FIND_FILE="/opt/Capricornus/config/gunicorn.py"
 FIND_STR="workers"
 if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
