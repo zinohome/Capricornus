896b36be9a4c7c4459e63185f516918875b4c022
zhangjun <ibmzhangjun@139.com>
2022-05-30 08:31:06 +0800

    update to v2.1.5

--- docker/bd_build/50_start_capricornus-h.sh
+++ docker/bd_build/50_start_capricornus-h.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-FIND_FILE="/opt/Capricornus/config/settings.ini"
-FIND_STR="settings"
-if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
-    echo "settings exist"
-else
-    cp /opt/Capricornus/config/settings_default.ini /opt/Capricornus/config/settings.ini
-fi
-FIND_FILE="/opt/Capricornus/config/gunicorn.py"
+FIND_FILE="/opt/Capricornus/config/hypercorn.py"
 FIND_STR="workers"
 if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
-    echo "gunicorn config exist"
+    echo "hypercorn config exist"
 else
     cp /opt/Capricornus/config/hypercorn_default.py /opt/Capricornus/config/hypercorn.py
 fi
